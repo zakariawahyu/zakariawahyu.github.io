145567aca712de5c994a5ffd2db5b2998fe439a8
Zakaria Wahyu <zakarianur6@gmail.com>
2020-03-17 23:34:12 +0700

    try contact

--- contact_process.php
+++ contact_process.php
@@ -1,37 +1,84 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+    // $to = "rockybd1995@gmail.com";
+    // $from = $_REQUEST['email'];
+    // $name = $_REQUEST['name'];
+    // $subject = $_REQUEST['subject'];
+    // $number = $_REQUEST['number'];
+    // $cmessage = $_REQUEST['message'];
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    // $headers = "From: $from";
+	// $headers = "From: " . $from . "\r\n";
+	// $headers .= "Reply-To: ". $from . "\r\n";
+	// $headers .= "MIME-Version: 1.0\r\n";
+	// $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
+    // $subject = "You have a message from your Bitmap Photography.";
 
-    $logo = 'img/logo.png';
-    $link = '#';
+    // $logo = 'img/logo.png';
+    // $link = '#';
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
+	// $body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+	// $body .= "<table style='width: 100%;'>";
+	// $body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+	// $body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+	// $body .= "</td></tr></thead><tbody><tr>";
+	// $body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
+	// $body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	// $body .= "</tr>";
+	// $body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+	// $body .= "<tr><td></td></tr>";
+	// $body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+	// $body .= "</tbody></table>";
+	// $body .= "</body></html>";
 
-    $send = mail($to, $subject, $body, $headers);
+	// $send = mail($to, $subject, $body, $headers);
+
+	$statusMsg = '';
+$msgClass = '';
+if(isset($_POST['submit'])){
+    // Get the submitted form data
+    $email = $_POST['email'];
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+    
+    // Cek apakah ada data yang belum diisi
+    if(!empty($email) && !empty($name) && !empty($subject) && !empty($message)){
+        
+        if(filter_var($email, FILTER_VALIDATE_EMAIL) === false){
+            $statusMsg = 'Please enter your valid email.';
+            $msgClassk = 'errordiv';
+        }else{
+            // Pengaturan penerima email dan subjek email
+            $toEmail = 'email@namadomain.com'; // Ganti dengan alamat email yang Anda inginkan
+            $emailSubject = 'Pesan website dari '.$name;
+            $htmlContent = '<h2> via Form Kontak Website</h2>
+                <h4>Name</h4><p>'.$name.'</p>
+                <h4>Email</h4><p>'.$email.'</p>
+                <h4>Subject</h4><p>'.$subject.'</p>
+                <h4>Message</h4><p>'.$message.'</p>';
+            
+            // Mengatur Content-Type header untuk mengirim email dalam bentuk HTML
+            $headers = "MIME-Version: 1.0" . "\r\n";
+            $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+            
+            // Header tambahan
+            $headers .= 'From: '.$name.'<'.$email.'>'. "\r\n";
+            
+            // Send email
+            if(mail($toEmail,$emailSubject,$htmlContent,$headers)){
+                $statusMsg = 'Pesan Anda sudah terkirim dengan sukses !';
+                $msgClass = 'succdiv';
+            }else{
+                $statusMsg = 'Maaf pesan Anda gagal terkirim, silahkan ulangi lagi.';
+                $msgClass = 'errordiv';
+            }
+        }
+    }else{
+        $statusMsg = 'Harap mengisi semua field data';
+        $msgClass = 'errordiv';
+    }
+}
 
 ?>
